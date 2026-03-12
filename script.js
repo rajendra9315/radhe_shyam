6b742351b08109ef15b98c697b537680611153aa
Rajendra 5 <rawatrajendra9315@gmail.com>
2026-03-12 13:38:47 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// button click redirect
+document.querySelectorAll(".join-btn").forEach(btn=>{
+    btn.addEventListener("click",function(){
+        window.location.href="https://t.me/YOURCHANNEL";
+    });
+});
