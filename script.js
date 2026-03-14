fb81d47f2d9135c5e5015d039f97682b7455b699
Rajendra 5 <rawatrajendra9315@gmail.com>
2026-03-14 21:55:35 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// button click redirect
-document.querySelectorAll(".join-btn").forEach(btn=>{
-    btn.addEventListener("click",function(){
-        window.location.href="https://t.me/YOURCHANNEL";
-    });
-});
